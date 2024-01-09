a7b71d055419d310af30c9892b505863d5cf3bc5
BatLdq <149933950+BatLdq@users.noreply.github.com>
2024-01-09 11:27:20 +0100

    Update TutorialRepository.java

--- src/main/java/com/bezkoder/spring/jpa/h2/repository/TutorialRepository.java
+++ src/main/java/com/bezkoder/spring/jpa/h2/repository/TutorialRepository.java
@@ -6,8 +6,25 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bezkoder.spring.jpa.h2.model.Tutorial;
 
+/**
+ * Interface representing a Spring Data JPA repository for managing Tutorial entities.
+ * Extends JpaRepository, providing CRUD operations for the Tutorial entity.
+ */
 public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
-  List<Tutorial> findByPublished(boolean published);
 
-  List<Tutorial> findByTitleContainingIgnoreCase(String title);
+    /**
+     * Retrieve tutorials based on their published status.
+     *
+     * @param published Published status to filter tutorials.
+     * @return List of tutorials with the specified published status.
+     */
+    List<Tutorial> findByPublished(boolean published);
+
+    /**
+     * Retrieve tutorials based on a case-insensitive partial match of their title.
+     *
+     * @param title Title (partially matching) to filter tutorials.
+     * @return List of tutorials with titles containing the specified substring.
+     */
+    List<Tutorial> findByTitleContainingIgnoreCase(String title);
 }
