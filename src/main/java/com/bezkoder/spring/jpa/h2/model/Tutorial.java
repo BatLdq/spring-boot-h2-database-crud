b8f2f2eb2144956f613ca0c307a858b35108149e
BatLdq <149933950+BatLdq@users.noreply.github.com>
2024-01-09 11:25:31 +0100

    Update Tutorial.java

--- src/main/java/com/bezkoder/spring/jpa/h2/model/Tutorial.java
+++ src/main/java/com/bezkoder/spring/jpa/h2/model/Tutorial.java
@@ -1,65 +1,130 @@
 package com.bezkoder.spring.jpa.h2.model;
 
 import jakarta.persistence.*;
-
+/**
+ * Entity class representing a Tutorial.
+ * Annotated with JPA annotations to map to the corresponding database table.
+ */
 @Entity
 @Table(name = "tutorials")
 public class Tutorial {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  private long id;
+    /**
+     * Unique identifier for the tutorial.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
 
-  @Column(name = "title")
-  private String title;
+    /**
+     * Title of the tutorial.
+     */
+    @Column(name = "title")
+    private String title;
 
-  @Column(name = "description")
-  private String description;
+    /**
+     * Description of the tutorial.
+     */
+    @Column(name = "description")
+    private String description;
 
-  @Column(name = "published")
-  private boolean published;
+    /**
+     * Published status of the tutorial.
+     */
+    @Column(name = "published")
+    private boolean published;
 
-  public Tutorial() {
+    /**
+     * Default constructor required by JPA.
+     */
+    public Tutorial() {
+        // Default constructor required by JPA.
+    }
 
-  }
+    /**
+     * Parameterized constructor to create a new tutorial.
+     *
+     * @param title       Title of the tutorial.
+     * @param description Description of the tutorial.
+     * @param published   Published status of the tutorial.
+     */
+    public Tutorial(String title, String description, boolean published) {
+        this.title = title;
+        this.description = description;
+        this.published = published;
+    }
 
-  public Tutorial(String title, String description, boolean published) {
-    this.title = title;
-    this.description = description;
-    this.published = published;
-  }
+    /**
+     * Getter method for retrieving the tutorial ID.
+     *
+     * @return ID of the tutorial.
+     */
+    public long getId() {
+        return id;
+    }
 
-  public long getId() {
-    return id;
-  }
+    /**
+     * Getter method for retrieving the tutorial title.
+     *
+     * @return Title of the tutorial.
+     */
+    public String getTitle() {
+        return title;
+    }
 
-  public String getTitle() {
-    return title;
-  }
+    /**
+     * Setter method for updating the tutorial title.
+     *
+     * @param title New title for the tutorial.
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-  public void setTitle(String title) {
-    this.title = title;
-  }
+    /**
+     * Getter method for retrieving the tutorial description.
+     *
+     * @return Description of the tutorial.
+     */
+    public String getDescription() {
+        return description;
+    }
 
-  public String getDescription() {
-    return description;
-  }
+    /**
+     * Setter method for updating the tutorial description.
+     *
+     * @param description New description for the tutorial.
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-  public void setDescription(String description) {
-    this.description = description;
-  }
+    /**
+     * Getter method for retrieving the published status of the tutorial.
+     *
+     * @return Published status of the tutorial.
+     */
+    public boolean isPublished() {
+        return published;
+    }
 
-  public boolean isPublished() {
-    return published;
-  }
+    /**
+     * Setter method for updating the published status of the tutorial.
+     *
+     * @param isPublished New published status for the tutorial.
+     */
+    public void setPublished(boolean isPublished) {
+        this.published = isPublished;
+    }
 
-  public void setPublished(boolean isPublished) {
-    this.published = isPublished;
-  }
-
-  @Override
-  public String toString() {
-    return "Tutorial [id=" + id + ", title=" + title + ", desc=" + description + ", published=" + published + "]";
-  }
+    /**
+     * Override of the toString method to provide a string representation of the tutorial.
+     *
+     * @return String representation of the tutorial.
+     */
+    @Override
+    public String toString() {
+        return "Tutorial [id=" + id + ", title=" + title + ", desc=" + description + ", published=" + published + "]";
+    }
 
 }
