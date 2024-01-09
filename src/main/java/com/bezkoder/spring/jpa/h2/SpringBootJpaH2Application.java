fcdc59b611047c1e674f00ab133cccea071e2a52
BatLdq <149933950+BatLdq@users.noreply.github.com>
2024-01-09 11:26:36 +0100

    Update SpringBootJpaH2Application.java

--- src/main/java/com/bezkoder/spring/jpa/h2/SpringBootJpaH2Application.java
+++ src/main/java/com/bezkoder/spring/jpa/h2/SpringBootJpaH2Application.java
@@ -3,11 +3,19 @@ package com.bezkoder.spring.jpa.h2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main class to bootstrap the Spring Boot application.
+ * Annotated with @SpringBootApplication, indicating that it is the main class and enabling auto-configuration.
+ */
 @SpringBootApplication
 public class SpringBootJpaH2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootJpaH2Application.class, args);
-	}
-
+    /**
+     * Main method to start the Spring Boot application.
+     *
+     * @param args Command line arguments.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJpaH2Application.class, args);
+    }
 }
